c8d0e48b3aeb892ba53139fbe5efc9b72ddc77a3
noralimanii <nnoralimani@live.com>
2024-03-07 12:42:30 -0500

    Alertcomponent and Action call

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React, { Fragment, fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import './App.css';
 //Redux
 import { Provider } from 'react-redux';
@@ -19,6 +20,7 @@ const App = () => {
             <Route exact path='/' element={<Landing />} />
           </Routes>
           <section className='container'>
+            <Alert />
             <Routes>
               <Route exact path='/register' element={<Register />} />
               <Route path='/login' element={<Login />} />
--- client/src/store.js
+++ client/src/store.js
@@ -3,13 +3,14 @@ import { thunk } from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware)
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
